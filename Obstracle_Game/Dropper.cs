7303ad95eafe9b1ccfd1cb437ad85238d7ee40b6
Minsu-Jung1999 <96643903+Minsu-Jung1999@users.noreply.github.com>
2023-06-22 20:05:38 -0700

    큐브를 움직여서 목표지점까지 가는 게임

---
+++ Obstracle_Game/Dropper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer renderer;
+    Rigidbody rigid;
+    [SerializeField] float timeToWait = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        rigid = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigid.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeToWait)
+        {
+            renderer.enabled = true;
+            rigid.useGravity = true;
+        }
+    }
+}
---
+++ Obstracle_Game/Scorer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private int hittingTimes = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision Other)
+    {
+        if(Other.gameObject.tag != "Hit")
+        {
+            hittingTimes++;
+        }
+        Debug.Log("You've bumped into thign this many times: " + hittingTimes);
+
+    }
+}
